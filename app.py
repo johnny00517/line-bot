afa99d2eb1577d1c8f55540b9721e3b9570577ce
Johnny Zhong <johnny00517@gmail.com>
2020-01-05 20:33:16 +0800

    update

--- app.py
+++ app.py
@@ -38,7 +38,13 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '你吃飯了嗎'
+    r = '很抱歉,我看不懂你說什麼'
+
+    if msg == 'hi':
+        r == 'hi'
+    elif msg == '你吃飯了嗎'
+        r == '吃了'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
